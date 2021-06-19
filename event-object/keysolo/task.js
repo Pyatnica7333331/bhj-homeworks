4fe95caeaba5b6c88141cee0467cd123f3603791
Pyatnica7333331 <alferova7333331@gmail.com>
2021-06-19 19:53:03 +0300

    task keysolo completed

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -15,16 +15,15 @@ class Game {
     this.winsElement.textContent = 0;
     this.lossElement.textContent = 0;
   }
-
+  
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
-  }
+    window.addEventListener("keyup", (event)=> { 
+      if (this.currentSymbol.textContent.toLowerCase() === event.key.toLowerCase()) {
+        this.success()
+      } else this.fail()
+    });
+    
+  };
 
   success() {
     this.currentSymbol.classList.add('symbol_correct');
