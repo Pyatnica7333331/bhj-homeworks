bf99c6cc651216a84bf913c76ffc632989f4a3e1
Pyatnica7333331 <alferova7333331@gmail.com>
2021-06-19 11:26:50 +0300

    task3 completed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,29 +1,16 @@
-const slides = document.getElementsByClassName("slider__item");
-const arrowNext = document.getElementsByClassName("slider__arrow_next");
-for (let item of arrowNext) {
-    item.onclick = function () {
-        const a = document.querySelector(".slider__item_active + .slider__item");
-        for (let i = 0; i < slides.length; i++) {
-            if (slides[i].classList.contains("slider__item_active")) {
-                slides[i].classList.remove("slider__item_active"); 
-                if (a) {
-                    a.classList.add("slider__item_active") 
-                } else slides[0].classList.add("slider__item_active");
-                
-            };
-        };
-    };   
-};
-const arrowPrev = document.getElementsByClassName("slider__arrow_prev");
-for (let item of arrowPrev) {
-    item.onclick = function () {
-        for (let i = 0; i < slides.length; i++) {
-            if (slides[i].classList.contains("slider__item_active")) {
-                slides[i].classList.remove("slider__item_active");
-                if (slides[i -1]) {
-                    slides[i -1].classList.add("slider__item_active")
-                } else slides[slides.length - 1].classList.add("slider__item_active")
-            };
-        };
-    };   
-};
+const slides = Array.from(document.getElementsByClassName("slider__item"));
+const arrowNext = document.querySelector(".slider__arrow_next");
+arrowNext.onclick = function () {
+    const index =  slides.findIndex((element) => {return element.classList.contains("slider__item_active")});
+    slides[index].classList.remove("slider__item_active"); 
+    slides[index + 1] ? 
+        slides[index + 1].classList.add("slider__item_active") : slides[0].classList.add("slider__item_active")    
+};   
+
+const arrowPrev = document.querySelector((".slider__arrow_prev"));
+arrowPrev.onclick = function () {
+    const index =  slides.findIndex((element) => {return element.classList.contains("slider__item_active")});
+    slides[index].classList.remove("slider__item_active"); 
+    slides[index - 1] ? 
+        slides[index - 1].classList.add("slider__item_active") : slides[slides.length - 1].classList.add("slider__item_active")    
+};   
